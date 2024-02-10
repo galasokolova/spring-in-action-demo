9faf0d014782621a4ee3690014ef159f665353bd
galasokolova <112399683+galasokolova@users.noreply.github.com>
2024-02-10 22:45:42 +0000

    Chapter 3: Spring Data JPA is considered

---
+++ src/main/java/demo/ch_3_jpa/converter/IngredientByIdConverter.java
@@ -0,0 +1,21 @@
+package demo.ch_3_jpa.converter;
+
+import demo.ch_3_jpa.data.IngredientRepository;
+import demo.ch_3_jpa.entity.Ingredient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+
+@Component
+public class IngredientByIdConverter implements Converter<String, Ingredient> {
+    private final IngredientRepository ingredientRepo;
+    @Autowired
+    public IngredientByIdConverter(IngredientRepository ingredientRepo) {
+        this.ingredientRepo = ingredientRepo;
+    }
+    @Override
+    public Ingredient convert(@NonNull String id) {
+        return ingredientRepo.findById(id).orElse(null);
+    }
+}
