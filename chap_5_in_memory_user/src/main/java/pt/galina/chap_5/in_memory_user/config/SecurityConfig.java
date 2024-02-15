1ba837d5633204f94b23da64096a7636d40a252e
galasokolova <112399683+galasokolova@users.noreply.github.com>
2024-02-15 23:39:49 +0000

    Chapter 5: Implemented Spring Security with InMemoryUserDetailsManager for user authentication

---
+++ chap_5_in_memory_user/src/main/java/pt/galina/chap_5/in_memory_user/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package pt.galina.chap_5.in_memory_user.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    /*
+     The in-memory user details service is convenient for testing purposes or for very simple applications.
+     */
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+        List<UserDetails> usersList = new ArrayList<>();
+        usersList.add(new User(
+                "buzz", encoder.encode("password"),
+                List.of(new SimpleGrantedAuthority("ROLE_USER"))));
+        usersList.add(new User(
+                "woody", encoder.encode("password"),
+                List.of(new SimpleGrantedAuthority("ROLE_USER"))));
+        return new InMemoryUserDetailsManager(usersList);
+    }
+}
---
+++ chap_5_in_memory_user/src/main/java/pt/galina/chap_5/in_memory_user/controller/HomeController.java
@@ -0,0 +1,13 @@
+package pt.galina.chap_5.in_memory_user.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+}
