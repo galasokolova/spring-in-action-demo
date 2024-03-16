8a80e1b64c2e81c648c8de42f9eff5364ca26c59
galasokolova <112399683+galasokolova@users.noreply.github.com>
2024-03-16 01:25:54 +0000

    created chap_8_spring_authorization_server

---
+++ chap_8_spring_authorization_server/src/main/java/pt/galina/Chap8SpringAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package pt.galina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap8SpringAuthorizationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap8SpringAuthorizationServerApplication.class, args);
+    }
+
+}
