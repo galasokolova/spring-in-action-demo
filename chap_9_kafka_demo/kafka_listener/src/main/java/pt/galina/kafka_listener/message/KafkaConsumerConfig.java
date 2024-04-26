d654fbc125e7867ba9e79a40752141c4da9b3332
galasokolova <112399683+galasokolova@users.noreply.github.com>
2024-04-26 20:47:18 +0100

    Added serializer and deserializer to chap_9_kafka

--- chap_9_kafka_demo/kafka_listener/src/main/java/pt/galina/kafka_listener/message/KafkaConsumerConfig.java
+++ chap_9_kafka_demo/kafka_listener/src/main/java/pt/galina/kafka_listener/message/KafkaConsumerConfig.java
@@ -1,13 +1,13 @@
 package pt.galina.kafka_listener.message;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import pt.galina.kafka_listener.deserialization.JsonPOJODeserializer;
 import pt.galina.kafka_listener.dto.TacoOrderDTO;
 
 import java.util.HashMap;
@@ -22,10 +22,9 @@ public class KafkaConsumerConfig {
         configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
         configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "tacocloud-listeners");
         configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        JsonDeserializer<TacoOrderDTO> deserializer = new JsonDeserializer<>(TacoOrderDTO.class);
-        deserializer.addTrustedPackages("*");
-        configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
-        return new DefaultKafkaConsumerFactory<>(configProps, new StringDeserializer(), deserializer);
+        // Используем кастомный десериализатор для значений
+        configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonPOJODeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(configProps, new StringDeserializer(), new JsonPOJODeserializer<>(TacoOrderDTO.class));
     }
 
     @Bean
--- chap_9_kafka_demo/kafka_producer/src/main/java/pt/galina/kafka_producer/message/KafkaProducerConfig.java
+++ chap_9_kafka_demo/kafka_producer/src/main/java/pt/galina/kafka_producer/message/KafkaProducerConfig.java
@@ -1,14 +1,14 @@
 package pt.galina.kafka_producer.message;
 
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.support.serializer.JsonSerializer;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import pt.galina.kafka_producer.dto.TacoOrderDTO;
+import pt.galina.kafka_producer.serialization.JsonPOJOSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -21,7 +21,8 @@ public class KafkaProducerConfig {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        // Используем кастомный сериализатор для значений
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonPOJOSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
