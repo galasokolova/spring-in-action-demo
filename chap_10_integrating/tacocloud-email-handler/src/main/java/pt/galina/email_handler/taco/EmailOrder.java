f43d6a2fa85120c196ca732bd83593430fa923d3
galasokolova <112399683+galasokolova@users.noreply.github.com>
2024-05-20 23:19:11 +0100

    EmailOrder is sent and received by tacocloud-api

--- chap_10_integrating/tacocloud-email-handler/src/main/java/pt/galina/email_handler/taco/EmailOrder.java
+++ chap_10_integrating/tacocloud-email-handler/src/main/java/pt/galina/email_handler/taco/EmailOrder.java
@@ -2,7 +2,33 @@ package pt.galina.email_handler.taco;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+
+//@Data
+//public class EmailOrder {
+//
+//    private final String email;
+//    private List<Taco> tacos = new ArrayList<>();
+//
+//    public void addTaco(Taco taco) {
+//        this. tacos.add(taco);
+//    }
+//
+//    public Taco findTacoByName(String tacoName) {
+//        if (tacos != null) {
+//            for (Taco taco : tacos) {
+//                if (taco != null && taco.getName().equalsIgnoreCase(tacoName)) {
+//                    return taco;
+//                }
+//            }
+//        }
+//        return null;
+//    }
+//}
+
 
 @Data
 public class EmailOrder {
@@ -10,8 +36,13 @@ public class EmailOrder {
     private final String email;
     private List<Taco> tacos = new ArrayList<>();
 
+    @JsonCreator // Указываем Jackson использовать этот конструктор для десериализации
+    public EmailOrder(@JsonProperty("email") String email) {
+        this.email = email;
+    }
+
     public void addTaco(Taco taco) {
-        this. tacos.add(taco);
+        this.tacos.add(taco);
     }
 
     public Taco findTacoByName(String tacoName) {
