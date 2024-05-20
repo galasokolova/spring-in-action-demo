a39a58257421c5380747399c13c7846a4a75e612
galasokolova <112399683+galasokolova@users.noreply.github.com>
2024-05-20 23:32:01 +0100

    Deleted comments and useless classes

--- chap_10_integrating/tacocloud-email-handler/src/main/java/pt/galina/email_handler/data/IngredientRepository.java
+++
@@ -1,7 +0,0 @@
-//package pt.galina.email_handler.data;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import pt.galina.email_handler.taco.Ingredient;
-//
-//public interface IngredientRepository extends JpaRepository<Ingredient, String> {
-//}
--- chap_10_integrating/tacocloud-email-handler/src/main/java/pt/galina/email_handler/taco/EmailOrder.java
+++ chap_10_integrating/tacocloud-email-handler/src/main/java/pt/galina/email_handler/taco/EmailOrder.java
@@ -7,28 +7,6 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
-//@Data
-//public class EmailOrder {
-//
-//    private final String email;
-//    private List<Taco> tacos = new ArrayList<>();
-//
-//    public void addTaco(Taco taco) {
-//        this. tacos.add(taco);
-//    }
-//
-//    public Taco findTacoByName(String tacoName) {
-//        if (tacos != null) {
-//            for (Taco taco : tacos) {
-//                if (taco != null && taco.getName().equalsIgnoreCase(tacoName)) {
-//                    return taco;
-//                }
-//            }
-//        }
-//        return null;
-//    }
-//}
-
 
 @Data
 public class EmailOrder {
