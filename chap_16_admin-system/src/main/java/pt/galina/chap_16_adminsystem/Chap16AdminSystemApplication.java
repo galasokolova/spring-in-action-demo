7414b07d1358b725ccd3d9db34f155bc458250bd
galasokolova <112399683+galasokolova@users.noreply.github.com>
2024-09-28 13:59:55 +0100

    added submodule admin-server to chap_16_admin-system

---
+++ chap_16_admin-system/src/main/java/pt/galina/chap_16_adminsystem/Chap16AdminSystemApplication.java
@@ -0,0 +1,13 @@
+package pt.galina.chap_16_adminsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap16AdminSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap16AdminSystemApplication.class, args);
+    }
+
+}
---
+++ chap_16_admin-system/src/test/java/pt/galina/chap_16_adminsystem/Chap16AdminSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.galina.chap_16_adminsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap16AdminSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
